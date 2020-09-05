03026c340a5a6128940370865b0368e6e03e859d
周口同学 <l790278104@163.com>
2020-09-05 08:43:10 +0800

    初尝git使用

---
+++ src/test/java/com/atguigu/MyTest.java
@@ -0,0 +1,26 @@
+package com.atguigu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void testDataSource(){
+
+        System.out.println(dataSource.getClass());
+    }
+
+
+}
